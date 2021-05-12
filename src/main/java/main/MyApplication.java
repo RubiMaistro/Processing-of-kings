d5a2e8f60dc9b9eff2fe8b5aaef6d53c38a5c7d6
Deák Ruben <rubendeak15@gmail.com>
2021-05-12 23:41:21 +0300

    Add MyApplication class for Main

---
+++ src/main/java/main/MyApplication.java
@@ -0,0 +1,19 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/launch.fxml"));
+        primaryStage.setTitle("Process Kings");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
